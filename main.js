432285169b54fbcb816b6ec49af5e50678962c74
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-02-01 21:03:31 -0600

    get the search gif update to work

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("test");
+(()=>{const e=document.getElementById("update-image"),t=document.getElementById("search-text");function n(t){fetch(`https://api.giphy.com/v1/gifs/translate?api_key=iBwgR73ftQriwkNWYpchMJu2xCFI2sog&s=${t}`,{mode:"cors"}).then((e=>e.json())).then((t=>{console.log(t.data.images.original.url),e.src=t.data.images.original.url}))}document.getElementById("update-button").addEventListener("click",(()=>{n(t.value)})),t.addEventListener("keydown",(e=>{"Enter"===e.key&&n(t.value)}))})();
